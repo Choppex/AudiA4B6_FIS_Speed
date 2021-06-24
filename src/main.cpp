c44fb4d466cf07655878a3ed0a8f1e0725f63f9b
krzysiek1998@gazeta.pl <krzysiek1998@gazeta.pl>
2021-06-24 22:11:28 +0200

    Add connection with mcp2515

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
+#include <mcp2515.h>
+
+MCP2515 mcp2515(10);
+
+struct can_frame speedMessage, firstRowMessage, secondRowMessage;
+
+void initFISFrames()
+{
+    firstRowMessage.can_id = 0x261;
+    firstRowMessage.can_dlc = 8;
+    secondRowMessage.can_id = 0x263;
+    secondRowMessage.can_dlc = 8;
+}
+
+void sendMessage()
+{
+    mcp2515.sendMessage(&firstRowMessage);
+    mcp2515.sendMessage(&secondRowMessage);
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_100KBPS, MCP_8MHZ);
+    mcp2515.setNormalMode();
+    initFISFrames();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
 }
